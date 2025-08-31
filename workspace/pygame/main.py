8e7f095d5252be7fb8fa8d858a6812ec987f3f85
Stephanie Nwankwo <stephybelle208@gmail.com>
2025-08-31 09:54:45 +0100

    feat: game loop

---
+++ workspace/pygame/main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+    while True:
+        # Handle quit event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  # Exit the game
+
+        # Fill the screen with black
+        screen.fill((0, 0, 0))
+
+        # Update the display
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
